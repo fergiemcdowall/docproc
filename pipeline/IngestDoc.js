2bc3911db84bbddf59634b9874dca6f3583573f3
Fergie McDowall <fergusmcdowall@gmail.com>
2016-10-19 14:33:53 +0200

    more robust autogenerating of ids

--- pipeline/IngestDoc.js
+++ pipeline/IngestDoc.js
@@ -10,13 +10,23 @@ module.exports = IngestDoc
 util.inherits(IngestDoc, Transform)
 IngestDoc.prototype._transform = function (doc, encoding, end) {
   var ingestedDoc = {
-    id: String(String(doc.id) || (Date.now() + '-' + ++this.i)),
     normalised: {},
     raw: JSON.parse(JSON.stringify(doc)),
     stored: {},
     tokenised: {},
     vector: {}
   }
+
+  // if there is no id, generate a a stringified one that is sorted
+  // by time (higher values more recent), so that a natural sort will
+  // yeild the most recent docs first
+  if (!doc.id) {
+    // TODO ++this.i should be left padded
+    ingestedDoc.id = Date.now() + '-' + ++this.i
+  } else {
+    ingestedDoc.id = String(doc.id)
+  }
+
   this.push(ingestedDoc)  // should this actually be stringified?
   return end()
 }
